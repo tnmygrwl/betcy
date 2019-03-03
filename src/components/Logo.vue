d83b19c8741f83f8c3094bdf2376734881a0ce7f
Tanmay Agrawal <tanmay7270@gmail.com>
2019-03-02 18:21:47 -0800

    add shitty logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="section">
-    <div class="logo"><img src="images/logo.png" width="180" srcset="images/logo-p-500.png 500w, images/logo-p-800.png 800w, images/logo-p-1080.png 1080w, images/logo.png 1270w" sizes="(max-width: 479px) 75vw, 180px" alt=""></div>
+    <div class="logo"><img src="images/logo.png" width="180" srcset="images/logo.png 500w, images/logo.png 800w, images/logo.png 1080w, images/logo.png 1270w" sizes="(max-width: 479px) 75vw, 180px" alt=""></div>
     <div class="hero-container w-container">
       <h1 class="h1">Bet a Macy's</h1>
       <h4><strong class="h3">Create a trackable bet with your friends for a Macy's product.</strong></h4>

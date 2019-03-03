b9fbe6524a4377a629cd48534d5d39275306dccf
Souradeep <dsouradeep2@gmail.com>
2019-03-03 08:50:48 -0800

    update

--- contracts/brutalexa.sol
+++ contracts/brutalexa.sol
@@ -20,17 +20,33 @@ contract brutalexa {
   mapping (address => uint[]) addresstobetidlist;
   mapping (address => uint[]) requestlist;
   mapping (uint => bet) betidtobet;
+  mapping (uint => string) betidtostring;
+  mapping (uint => address) betopp;
 
  function next_id() internal{
   b_id_count+=1;
  }
 
- function create_promise(address sec, string bettext, string date, address wit1, address wit2, address wit3,uint productid) public {
+ function create_promise(address sec, string bettext, uint productid) public {
    next_id();
-   betidtobet[b_id_count] = bet(b_id_count,msg.sender,sec,bettext,date,wit1,wit2,wit3,0,productid);
-   addresstobet[msg.sender]= bet(b_id_count,msg.sender,sec,bettext,date,wit1,wit2,wit3,0,productid);
+   betidtobet[b_id_count] = bet(b_id_count,msg.sender,sec,bettext,"03/03/2019",sec,sec,sec,0,productid);
+   addresstobet[msg.sender]= bet(b_id_count,msg.sender,sec,bettext,"03/03/2019",sec,sec,sec,0,productid);
    addresstobetidlist[msg.sender].push(b_id_count);
    requestlist[sec].push(b_id_count);
+ }
+
+ function create_init(address sec,string bettext) public {
+   next_id();
+   betidtobet[b_id_count] = bet(b_id_count,msg.sender,sec,bettext,"03/03/2019",sec,sec,sec,0,0);
+   betidtostring[b_id_count]=bettext;
+   betopp[b_id_count]=sec;
+ }
+
+ function finish_init(uint prodid) public {
+   betidtobet[b_id_count] = bet(b_id_count,msg.sender,betopp[b_id_count],betidtostring[b_id_count],"03/03/2019",betopp[b_id_count],betopp[b_id_count],betopp[b_id_count],0,prodid);
+   addresstobet[msg.sender] = bet(b_id_count,msg.sender,betopp[b_id_count],betidtostring[b_id_count],"03/03/2019",betopp[b_id_count],betopp[b_id_count],betopp[b_id_count],0,prodid);
+   addresstobetidlist[msg.sender].push(b_id_count);
+   requestlist[betopp[b_id_count]].push(b_id_count);
  }
 
  function show_pending_requests() public view returns(uint[]) {
@@ -38,7 +54,7 @@ contract brutalexa {
  }
 
  function show_validity(uint b_id) public view returns(uint) {
-   return validity;
+  return betidtobet[b_id].validity;
  }
  function showbets(uint b_id) public view returns(uint bet_id,address creator,address player,string text,string due_date,address witness1,address witness2,address witness3,uint validity,uint productid) {
    bet_id = b_id;
